e5e944061161145232319380f072500111cf73f5
dezokkk <khangnguyenhuy123@gmail.com>
2025-12-05 13:31:50 +0700

    Add full source code

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,11 +1,11 @@
 const MyConstants = {
-    DB_SERVER: 'cluster01.lzjgfli.mongodb.net',
-    DB_USER: 'chauhoangkha',
-    DB_PASS: '123',
-    DB_DATABASE: 'shoppingonline',
-    JWT_SECRET: '<jwt_secret>',
-    JWT_EXPIRES: '351636000000', // in milliseconds
-    EMAIL_USER: '<email_user>', // gmail service
-    EMAIL_PASS: '<app_pass>'
-  };
-  module.exports = MyConstants;
+  DB_SERVER: 'cluster01.8xxyisb.mongodb.net',
+  DB_USER: 'luongvuhong',
+  DB_PASS: '15122002',
+  DB_DATABASE: 'shoppingOnline',
+  JWT_SECRET: '4431',
+  JWT_EXPIRES: '31565890000000', // in milliseconds
+  EMAIL_USER: 'luongvuhong2202@gmail.com', // gmail service
+  EMAIL_PASS: 'eltuloolnrbvwzhy'
+};
+module.exports = MyConstants;
